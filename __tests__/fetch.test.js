162ab306a48c1ef99ed72af2954ab0e0deba4d33
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-07-02 10:10:24 +0200

    tests: add process.env test

--- __tests__/fetch.test.js
+++ __tests__/fetch.test.js
@@ -2,25 +2,32 @@ const fetch = require("jest-fetch-mock");
 
 jest.setMock("node-fetch", fetch);
 
-jest.mock("simple-oauth2", () => ({
-  create: () => ({
-    clientCredentials: {
-      getToken: () => {}
-    },
-    accessToken: {
-      create: () => ({
-        token: { access_token: 1234 }
-      })
-    }
-  })
+fetch.mockResponse(JSON.stringify({ message: "YATTA!" }));
+
+process.env = {
+  OAUTH_CLIENT_ID: "test-client-id",
+  OAUTH_CLIENT_SECRET: "test-client-secret"
+};
+
+const mockCreate = jest.fn(() => ({
+  clientCredentials: {
+    getToken: () => {}
+  },
+  accessToken: {
+    create: () => ({
+      token: { access_token: 1234 }
+    })
+  }
 }));
 
-fetch.mockResponse(JSON.stringify({ message: "YATTA!" }));
+jest.mock("simple-oauth2", () => ({
+  create: mockCreate
+}));
 
 const Client = require("..");
 
 describe("client.fetch", () => {
-  test("make a call with correct token and parameters", async () => {
+  test("send correct API parameters", async () => {
     const client = new Client();
     const res = await client.fetch({
       path: "consult/code/tableMatieres",
@@ -37,5 +44,21 @@ describe("client.fetch", () => {
       `${client.apiHost}/consult/code/tableMatieres`
     );
     expect(firstCall[1]).toMatchSnapshot();
+  });
+
+  test("send oauth credentials with process.env", async () => {
+    const client = new Client();
+    const res = await client.fetch({
+      path: "consult/code/tableMatieres",
+      method: "POST",
+      params: {
+        date: 1234,
+        sctId: "",
+        textId: "LEGITEXT000006072050"
+      }
+    });
+
+    expect(mockCreate.mock.calls.length).toEqual(4);
+    expect(mockCreate.mock.calls[0][0]).toMatchSnapshot();
   });
 });
